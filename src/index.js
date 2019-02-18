2b07be7ec77cf74a288a84222a30ac7ed71fb3b7
Khánh Vương <khanhvuong8939@gmail.com>
2019-02-18 21:37:33 +0700

    integrate Redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, compose } from 'redux';
+import AppReducer from './reducers/index';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(AppReducer, composeEnhancer(applyMiddleware(thunk)))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
 
 serviceWorker.unregister();
