1539dc4a0f609158f5019cc9b16b95015e00aad6
vuong.tk <vuong.tk@gnt.com.vn>
2019-02-18 15:47:27 +0700

    setup router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage'
+import LoginPage from './components/pages/LoginPage'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
-        <nav class="navbar navbar-inverse">
-          <a class="navbar-brand" href="#">test</a>
-          <ul class="nav navbar-nav">
-            <li class="active">
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Link</a>
-            </li>
-          </ul>
-        </nav>
-        
-      </div>
+      <Router>
+        <div>
+          <nav className="navbar navbar-inverse">
+            <ul className="nav navbar-nav">
+              <li className="active">
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route path='/' exact component={HomePage} />
+          <Route path='/login' exact component={LoginPage} />
+        </div>
+      </Router>
     );
   }
 }
