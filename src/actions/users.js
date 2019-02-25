461b9b2af8c09aaaf6168e54509a3f64ce49734c
vuong.tk <vuong.tk@gnt.com.vn>
2019-02-25 17:01:19 +0700

    Refactore code

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,4 +1,7 @@
 import api from "../api/api";
 import { userLoggedIn } from "./auth";
 
-export const signup = data => dispatch =>{ console.log(data); return api.user.signup(data).then(res => dispatch(userLoggedIn(res)))}
+export const signup = data => dispatch => api.user.signup(data).then(user => {
+    localStorage.bookwormJWT = user.token
+    dispatch(userLoggedIn(user))
+})
